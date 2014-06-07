86aa42a76d893b9d39c7cc4bb12dd8703516fed5
Bryan Garza <brygarza@gmail.com>
2014-06-07 04:11:11 -0700

    Add navbar and intro text

--- src/gather/views.clj
+++ src/gather/views.clj
@@ -2,26 +2,72 @@
   (:use hiccup.core)
   (:require [hiccup.page :as page]
             [hiccup.element :as element]
-            [clojure.string :as string]))
+            [clojure.string :refer [capitalize]]))
 
-(defn app [title & body]
+(defn common [title & body]
   (html
     [:head
-      [:meta {:charset "utf-8"}]
-      [:meta {:http-equiv "X-UA-Compatible" :content "IE-Edge"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-      [:title title]
-      (page/include-css "css/bootstrap.min.css")
-      (page/include-js "js/bootstrap.min.js")]
-    [:body body]))
+     [:meta {:charset "utf-8"}]
+     [:meta {:http-equiv "X-UA-Compatible" :content "IE-Edge"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+     [:meta {:name "description" :content "make a page for your event"}]
+     [:meta {:name "author" :content "Bryan Garza"}]
+     [:title title]
+     (page/include-css "css/bootstrap.min.css"
+                       "font-awesome/css/font-awesome.min.css"
+                       "fonts/lora.css"
+                       "fonts/montserrat.css"
+                       "css/grayscale.css")]
+    [:div.container
+     [:body#page-top {:data-spy "scroll" :data-target ".navbar-custom"} body]]
+    (page/include-js "js/jquery.min.js"
+                     "js/bootstrap.min.js"
+                     "js/jquery.easing.min.js"
+                     "js/grayscale.js")))
 
 (defn index []
-  (app
+  (common
     "Home"
-    [:h1 "Gather"]
-    [:h2 "Create an information page for your event, no signup required."]
-    (map (fn [w]
-           [:div.input-group.input-group-lg
-           [:input.form-control {:type w :placeholder (string/capitalize w)}]])
-         ["title" "location" "date" "time"])
-    [:button.btn.btn-default {:type "submit"} "Submit"]))
+    [:nav.navbar.navbar-custom.navbar-fixed-top {:role "navigation"}
+     [:div.container
+
+      [:div.navbar-header.page-scroll
+       [:button.navbar-toggle {:type "button"
+                               :data-toggle "collapse"
+                               :data-target ".navbar-main-collapse"}
+        [:i.fa.fa-bars]]
+       [:a.navbar-brand {:href "#page-top"}
+        [:i.fa.fa-paper-plane] [:span.light "Start"] "Bootstrap"]]
+
+      ; Collect the nav links, forms, and other content for toggling
+      [:div.collapse.navbar-collapse.navbar-right.navbar-main-collapse
+       [:ul.nav.navbar-nav
+        ; Hidden li included to remove active class from about link when
+        ; scrolled up past section
+        [:li.hidden (element/link-to "#page-top")]
+        [:li.page-scroll (element/link-to "#create" "Create")]
+        [:li.page-scroll (element/link-to "#events" "Events")]
+        [:li.page-scroll (element/link-to "#contact" "Contact")]]]]]
+
+    [:section.intro
+     [:div.intro-body
+      [:div.container
+       [:div.row
+        [:div.col-md-8.col-md-offset-2
+         [:h1.brand-heading "Gather"]
+         [:p.intro-text "Make a page for your event. No signups, no mess.
+                         We'll take care of the details."]
+         [:div.page-scroll
+          [:a.btn.btn-circle {:href "#create"}
+           [:i.fa.fa-angle-double-down.animated]]]]]]]]
+
+    ; [:div.col-md-2]
+    ; [:div.col-md-8
+    ;  (map (fn [w]
+    ;         [:div.input-group.input-group-lg
+    ;          [:input.form-control {:type w :placeholder (capitalize w)}]])
+    ;       ["title" "location" "date" "time"])
+    ;  [:textarea.form-control.input-lg {:rows "5":placeholder "Description"}]
+    ;  [:button.btn.btn-primary {:type "submit"} "Submit"]]
+    ; [:div.col-md-2]
+    ))
